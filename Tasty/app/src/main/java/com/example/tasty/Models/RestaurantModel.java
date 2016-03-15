b6e85b2bd5504068c125dbb198b7328b9f4c3203
Carlos Fabela <cafabela@gmail.com>
2016-03-14 19:32:28 -0600

    -A Android: Adding class for getting geolocation or str address

--- Tasty/app/src/main/java/com/example/tasty/Models/RestaurantModel.java
+++ Tasty/app/src/main/java/com/example/tasty/Models/RestaurantModel.java
@@ -6,38 +6,44 @@ public class RestaurantModel {
     private double latitude;
 
     private String name;
-    private int zipCode;
+    private String zipCode;
     private String state;
     private String country;
-    private String direction;
+    private String address;
+    private String city;
 
     private EnumRestaurants restaurantType;
 
-    public RestaurantModel(String name, int zipCode, String state, String country, String direction) {
-        this.name = name;
-        this.zipCode = zipCode;
-        this.state = state;
-        this.direction = direction;
+
+    public RestaurantModel(){
 
     }
 
-    public RestaurantModel(String name, int zipCode, String state, String country, String direction,
+    public RestaurantModel(String name, String zipCode, String state, String country, String address) {
+        this.name = name;
+        this.zipCode = zipCode;
+        this.state = state;
+        this.address = address;
+
+    }
+
+    public RestaurantModel(String name, String zipCode, String state, String country, String address,
                            double latitude, double longitude) {
         this.name = name;
         this.zipCode = zipCode;
         this.state = state;
         this.country = country;
-        this.direction = direction;
+        this.address = address;
         this.latitude = latitude;
         this.longitude = longitude;
     }
 
-    public RestaurantModel(String name, String state, String country, String direction)
+    public RestaurantModel(String name, String state, String country, String address)
     {
         this.name = name;
         this.state = state;
         this.country = country;
-        this.direction = direction;
+        this.address = address;
     }
 
     public double getLatitude(){
@@ -64,11 +70,11 @@ public class RestaurantModel {
         this.name = name;
     }
 
-    public int getZipCode(){
+    public String getZipCode(){
         return zipCode;
     }
 
-    public void setZipCode(int zipCode){
+    public void setZipCode(String zipCode){
         this.zipCode = zipCode;
     }
 
@@ -80,8 +86,8 @@ public class RestaurantModel {
         this.state = state;
     }
 
-    public String getDirection(){
-        return direction;
+    public String getAddress(){
+        return address;
     }
 
     public String getCountry(){
@@ -92,8 +98,8 @@ public class RestaurantModel {
         this.country = country;
     }
 
-    public void setDirection(String direction){
-        this.direction = direction;
+    public void setAddress(String address){
+        this.address = address;
     }
 
     public EnumRestaurants getRestaurantType()
@@ -103,5 +109,13 @@ public class RestaurantModel {
 
     public void setRestaurantType(EnumRestaurants restaurantType){
         this.restaurantType = restaurantType;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
     }
 }
--- Tasty/app/src/main/java/com/example/tasty/Utils/Util.java
+++ Tasty/app/src/main/java/com/example/tasty/Utils/Util.java
@@ -1,13 +1,19 @@
 package com.example.tasty.Utils;
 
 
+import android.location.Geocoder;
 import android.support.design.widget.Snackbar;
 import android.widget.LinearLayout;
+import android.content.Context;
+import android.location.Address;
 
+import com.example.tasty.Models.RestaurantModel;
 import com.google.android.gms.maps.model.LatLng;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.ArrayList;
+import java.util.Locale;
 
 public final class Util {
 
@@ -21,6 +27,55 @@ public final class Util {
 
         return snack;
     }
+
+    public static LatLng getCoordFromAddress(Context context, String address){
+        Geocoder geocoder = new Geocoder(context, Locale.getDefault());
+        LatLng latLng = null;
+
+        try {
+            List<Address> lstAddress = geocoder.getFromLocationName(address,1);
+            if(lstAddress.size() > 0){
+                latLng = new LatLng(lstAddress.get(0).getLatitude(),
+                        lstAddress.get(0).getLongitude());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return latLng;
+    }
+
+    public static RestaurantModel getAddressFromGeo(Context context, LatLng latLng){
+        Geocoder geocoder = new Geocoder(context, Locale.getDefault());
+        RestaurantModel restaurantModel = null;
+
+        List<Address> lstAddress = null;
+        try {
+            lstAddress = geocoder.getFromLocation(latLng.latitude,latLng.longitude,1);
+            if(lstAddress.size() > 0){
+                Address address = lstAddress.get(0);
+                String strAddress = address.getAddressLine(0);
+                String city = address.getLocality();
+                String state = address.getAdminArea();
+                String country = address.getCountryName();
+                String zipCode = address.getPostalCode();
+                String name = address.getFeatureName();
+
+                restaurantModel = new RestaurantModel();
+                restaurantModel.setName(name);
+                restaurantModel.setAddress(strAddress);
+                restaurantModel.setCity(city);
+                restaurantModel.setState(state);
+                restaurantModel.setCountry(country);
+                restaurantModel.setZipCode(zipCode);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        
+        return  restaurantModel;
+    }
+
     public static class PolyUtil{
         public static List<LatLng> decode(final String encodedPath) {
             int len = encodedPath.length();
